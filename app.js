85223d256e69c5c41f30b85ad8d3c94182bc3dee
Salvatore <workstudioms@gmail.com>
2025-02-05 16:32:29 +0100

    posts array and images , add. Posts list bonus, complete.

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const postsRouter = require('./routers/posts');
+const postsArray = require('./arraypost/posts');
 
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('Server del mio blog')
@@ -10,6 +13,7 @@ app.get('/', (req, res) => {
 
 
 app.use("/posts", postsRouter);
+app.use("/arrayposts", postsArray);
 
 
 app.listen(port, () => {
